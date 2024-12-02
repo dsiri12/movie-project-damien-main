bf3463a1f8394703dc052871b8e7c743fe6852b1
Damien Siri <dsiri12@gmail.com>
2024-12-02 18:57:00 -0500

    another recent change I've made to the alternative component for my movie website project

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <Nav background='#242424' />
       <HeroHeader title='Space-Themed Movies' image='/moon-and-earth-header.png' subtitle='Check out these movies that immerse you into an adventure within interplanetary space!' height={400} />
       <MovieSelectionSlider itemsPerSlide={2} items={[{image: '/moon-and-earth-header.png', title: 'Movie One'}, {image: '/moon-and-earth-header.png', title: 'Movie Two'}, {image: '/moon-and-earth-header.png', title: 'Movie Three'}, {image: '/moon-and-earth-header.png', title: 'Movie Four'} ]} />
-      <NearbyTheatersContainer items={[{title: 'Movie Theater 1', subtitle: '401 Passaic Drive New York, NY 010101'}]} />
+      <NearbyTheatersContainer items={[{ title: 'Movie Theater 1', subtitle: '401 Passaic Drive New York, NY 010101' }]} />
       <BottomFooter background='#242424' />
     </>
   );
--- src/components/NearbyTheaters/NearbyTheatersContainer.tsx
+++ src/components/NearbyTheaters/NearbyTheatersContainer.tsx
@@ -8,11 +8,14 @@ export interface NearbyTheatersContainerProps {
 function NearbyTheatersContainer(props: NearbyTheatersContainerProps) {
     return (
         <div className='nearby-theaters-container'>
-            <div className='nearby-theaters-content'>
-               {props.items.map((_item, _index) => {
-                    return <NearbyTheatersItem title={'Movie Theater 1'} subtitle={'401 Passaic Drive New York, NY 010101'} width={250} height={250}/>
-                })} 
+            <div className='nearby-theaters-item-card'>
+                <div className='nearby-theaters-content'>
+                    {props.items.map((_item, _index) => {
+                        return <NearbyTheatersItem title={'Movie Theater 1'} subtitle={'401 Passaic Drive New York, NY 10101'} width={250} height={250}/>
+                    })} 
+                </div>
             </div>
+            
         </div>
     )
 }
