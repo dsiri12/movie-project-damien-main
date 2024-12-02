bdeb422484700a98fcd72a471c8b777881f2b2e4
Damien Siri <dsiri12@gmail.com>
2024-12-01 22:38:14 -0500

    change I've made on the recent text I've added to the bottom footer of my movie website project tonight

--- src/components/BottomFooter/BottomFooter.tsx
+++ src/components/BottomFooter/BottomFooter.tsx
@@ -9,6 +9,7 @@ function BottomFooter(props: BottomFooterProps) {
         <nav className='bottom-footer' style={{background: props.background}}>
             <div className='bottom-footer-content'>
                 <div className="bottom-footer-links">
+                    <h4 className='text'>© 2024 COPYRIGHT</h4>
                     <a className="button" href="/support">SUPPORT</a>
                     <a className="button" href="/privacy">PRIVACY</a>
                     <a className="button" href="/terms">TERMS OF USE</a>
