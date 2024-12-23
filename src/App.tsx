945ab1b4e81ae4f49f51e2a5e636d6b4ae14cd60
Damien Siri <dsiri12@gmail.com>
2024-12-23 18:40:39 -0500

    new movie slider component update on my movie website project

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,19 @@ function App() {
     <>
       <Nav background='#242424' />
       <HeroHeader title='Space-Themed Movies' image='/moon-and-earth-header.png' subtitle='Check out these movies that immerse you into an adventure within interplanetary space!' height={400} />
-      <MovieSelectionSlider itemsPerSlide={2} items={[{image: '/moon-and-earth-header.png', title: 'Movie One'}, {image: '/moon-and-earth-header.png', title: 'Movie Two'}, {image: '/moon-and-earth-header.png', title: 'Movie Three'}, {image: '/moon-and-earth-header.png', title: 'Movie Four'} ]} />
+      <MovieSelectionSlider items={[{
+        image: '/moon-and-earth-header.png', title: 'Movie One',
+        itemsPerSlide: 0
+      }, {
+        image: '/moon-and-earth-header.png', title: 'Movie Two',
+        itemsPerSlide: 0
+      }, {
+        image: '/moon-and-earth-header.png', title: 'Movie Three',
+        itemsPerSlide: 0
+      }, {
+        image: '/moon-and-earth-header.png', title: 'Movie Four',
+        itemsPerSlide: 0
+      } ]} />
       <NearbyTheatersContainer items={[{ title: 'Movie Theater 1', subtitle: '401 Passaic Drive New York, NY 010101' }]} />
       <BottomFooter background='#242424' />
     </>
--- src/components/MovieSelectionSlider/MovieSelectionSlider.tsx
+++ src/components/MovieSelectionSlider/MovieSelectionSlider.tsx
@@ -1,30 +1,56 @@
 import MovieSelectionSliderItem, { MovieSelectionSliderItemProps } from "./MovieSelectionSliderItem";
 import './MovieSelectionSlider.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 interface MovieSelectionSliderProps {
-    items: MovieSelectionSliderItemProps[];
     itemsPerSlide: number;
+    items: MovieSelectionSliderItemProps[];
 }
 
 function MovieSelectionSlider(props: MovieSelectionSliderProps) {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
     return (
-        <div className='movie-slider-container'>
-            <div className='left-button navigation'>
-                <button className='left-arrow-button'>
-                    <img src="leftArrowButton.png" alt="LeftArrowButton" />
-                </button> 
-            </div>
-            <div className='right-button navigation'>
-                <button className='right-arrow-button'>
-                    <img src="rightArrowButton.png" alt="RightArrowButton" />
-                </button>
-            </div>
             <div className='movie-slider-wrapper'>
-                {props.items.map((item, index) => {
+                <Carousel responsive={responsive} swipeable={false}
+  draggable={false}
+  showDots={true}
+  ssr={true} // means to render carousel on server-side.
+  infinite={true}
+  autoPlaySpeed={1000}
+  keyBoardControl={true}
+  customTransition="all .5"
+  transitionDuration={500}
+  containerClass="carousel-container"
+  removeArrowOnDeviceType={["tablet", "mobile"]}
+  dotListClass="custom-dot-list-style"
+  itemClass="carousel-item-padding-40-px">
+                   {props.items.map((item, index) => {
                     return <MovieSelectionSliderItem itemsPerSlide={props.itemsPerSlide} image={item.image} title={item.title} key={item.title}/>
-                })}
+                })} 
+                
+                </Carousel>
             </div>
-        </div>
+        
     )
 }
 
